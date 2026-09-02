cbe55f43667934c0bb2b7a4ac0c2c4346717a1d9
stefano liga <sliga@consulenti.fideuram.it>
2026-09-02 23:19:07 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/example/alessialiga/AlessialigaApplication.java
@@ -0,0 +1,13 @@
+package com.example.alessialiga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlessialigaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlessialigaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/alessialiga/AlessialigaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alessialiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlessialigaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
